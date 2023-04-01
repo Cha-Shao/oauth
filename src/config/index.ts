f5c52d6702d7a97f560b18d4e902f0ffb78ede5e
Cha-Shao <yrchashao@outlook.com>
2023-04-02 05:43:35 +0800

    fix database

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,8 +12,6 @@ export const {
   DB_HOST,
   DB_PORT,
   DB_DATABASE,
-  DB_USER,
-  DB_PASS,
   // TOKEN
   SECRET_KEY,
   // LOG
--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,11 +1,11 @@
-import { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASS } from '@config';
+import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
 export const dbConnection = {
   url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   options: {
-    user: DB_USER,
-    pass: DB_PASS,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    user: '',
+    pass: '',
   },
 };
